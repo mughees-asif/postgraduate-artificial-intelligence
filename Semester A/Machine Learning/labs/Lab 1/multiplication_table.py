af64b9b29881f999be8b3192980bc97233e6a148
Mughees Asif <mughees460@gmail.com>
2021-10-10 14:41:48 +0100

    Contributing guidelines introduced

--- Semester A/Machine Learning/labs/Lab 1/multiplication_table.py
+++ Semester A/Machine Learning/labs/Lab 1/multiplication_table.py
@@ -1,18 +1,13 @@
 start = 1
 end = 6
 
-print('----- Q1(a): Print a small multiplication square ------')
 # Q1(a): Print a small multiplication square
-start = 1
-end = 6
-
 for row in range(start, end):
     for col in range(start, end):
         res = row * col
         print(res, end="\t")
     print()
 
-print('\n----- Q1(b): Even numbers floored to 0 ------')
 # Q1(b): Print a small multiplication square, even numbers floored to 0
 for row in range(start, end):
     for col in range(start, end):
